d4094585c65d2e5bb044f8e1fe3b969c46cf4ba7
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2019-11-23 10:49:34 -0300

    Projeto da aula

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Portal m_OtherPortal;
+    public Transform m_Spawner;
+    public float m_Impulse = 5.0f;
+    public float m_Multiply = 1.0f;
+    public Vector3 m_Direction = Vector3.forward;
+    public bool m_Active = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!m_Active) return;
+        if (m_OtherPortal == null) return;
+        if (!m_OtherPortal.m_Active) return;
+
+        var direction = m_OtherPortal.m_Spawner.TransformDirection(m_OtherPortal.m_Direction);
+
+        other.transform.position = m_OtherPortal.m_Spawner.position;
+        other.transform.rotation = Quaternion.LookRotation(direction);
+
+        Teleportable teleportable = other.GetComponent<Teleportable>();
+        teleportable.AddForce(direction, m_Impulse, m_Multiply);
+    }
+}
---
+++ Assets/Scripts/Teleportable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportable : MonoBehaviour
+{
+    private Rigidbody m_Body;
+    private float m_Speed;
+
+    private void Awake()
+    {
+        m_Body = GetComponent<Rigidbody>();
+    }
+
+    private void LateUpdate()
+    {
+        m_Speed = m_Body.velocity.magnitude;
+    }
+
+    public void AddForce(Vector3 direction, float impulse, float multiply)
+    {
+        m_Body.velocity = Vector3.zero;
+        m_Body.AddForce(direction * m_Speed * multiply + direction * impulse, ForceMode.VelocityChange);
+    }
+}
